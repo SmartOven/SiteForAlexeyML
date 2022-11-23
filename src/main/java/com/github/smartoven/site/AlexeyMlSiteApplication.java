ec8257f492b07992203f4145b261834f3f63df2b
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-11-23 15:03:31 +0300

    Initial commit

---
+++ src/main/java/com/github/smartoven/site/AlexeyMlSiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.smartoven.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexeyMlSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexeyMlSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/smartoven/site/AlexeyMlSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smartoven.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexeyMlSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
